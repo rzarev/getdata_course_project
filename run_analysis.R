0404dacf33c6a2b88812d4a27c9df26e88a6a1aa
Rumen Zarev <rumen.zarev@gmail.com>
2014-09-15 13:10:43 -0700

    Add some stub comments for each step.

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,7 @@
+# Step 0
+# Anything not explicitly mentioned in the assignment.
+# ====================================================
+
 # File names
 remote_url <- paste0("https://d396qusza40orc.cloudfront.net/",
                      "getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip")
@@ -17,3 +21,25 @@ activity_labels <- read.table(paste0(data_folder, "activity_labels.txt"),
 feature_labels <- read.table(paste0(data_folder, "features.txt"),
                              header = FALSE, stringsAsFactors = FALSE,
                              col.names = c("col_number", "labe"))
+
+# Step 1
+# Merges the training and the test sets to create one data set.
+# =============================================================
+
+# Step 2
+# Extracts only the measurements on the mean and standard deviation
+# for each measurement. 
+# =================================================================
+
+# Step 3
+# Uses descriptive activity names to name the activities in the data set.
+# =======================================================================
+
+# Step 4
+# Appropriately labels the data set with descriptive variable names.
+# ==================================================================
+
+# Step 5
+# From the data set in step 4, creates a second, independent tidy data
+# set with the average of each variable for each activity and each subject.
+# =========================================================================
