b16d9bd9b42668367ad8851fc7041cc8e066990b
Rumen Zarev <rumen.zarev@gmail.com>
2014-09-15 13:02:05 -0700

    Implement: read in the labels for the data.

--- run_analysis.R
+++ run_analysis.R
@@ -9,3 +9,11 @@ if (!file.exists(local_zipfile))
     download.file(url = remote_url, destfile = local_zipfile, method = "wget")
 if (!file.exists(data_folder))
     unzip(local_zipfile)
+
+# Load all the data labels
+activity_labels <- read.table(paste0(data_folder, "activity_labels.txt"),
+                              header = FALSE, stringsAsFactors = FALSE,
+                              col.names = c("level", "label"))
+feature_labels <- read.table(paste0(data_folder, "features.txt"),
+                             header = FALSE, stringsAsFactors = FALSE,
+                             col.names = c("col_number", "labe"))
