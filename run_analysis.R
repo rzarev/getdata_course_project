333b784b8c2e9cfca2f9421cba50ab9beb3245c1
Rumen Zarev <rumen.zarev@gmail.com>
2014-09-15 13:28:50 -0700

    Implement step 1 (read and merge data).

--- run_analysis.R
+++ run_analysis.R
@@ -26,6 +26,22 @@ feature_labels <- read.table(paste0(data_folder, "features.txt"),
 # Merges the training and the test sets to create one data set.
 # =============================================================
 
+sub_train <- read.table(paste0(data_folder, "train/subject_train.txt"),
+                        header = FALSE, col.names = c("subject"))
+sub_test  <- read.table(paste0(data_folder, "test/subject_test.txt"),
+                        header = FALSE, col.names = c("subject"))
+X_train   <- read.table(paste0(data_folder, "train/X_train.txt"),
+                        header = FALSE, col.names = feature_labels$label)
+X_test    <- read.table(paste0(data_folder, "test/X_test.txt"),
+                        header = FALSE, col.names = feature_labels$label)
+y_train   <- read.table(paste0(data_folder, "train/y_train.txt"),
+                        header = FALSE, col.names = c("activity"))
+y_test    <- read.table(paste0(data_folder, "test/y_test.txt"),
+                        header = FALSE, col.names = c("activity"))
+
+raw_data <- rbind(cbind(sub_train, X_train, y_train),
+                  cbind(sub_test,  X_test,  y_test))
+
 # Step 2
 # Extracts only the measurements on the mean and standard deviation
 # for each measurement. 
